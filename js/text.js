0dfc941ea990805843d2de8567b86329fb399c84
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-09-16 04:30:56 +0900

    :+1:Tweak

--- js/text.js
+++ js/text.js
@@ -23,52 +23,24 @@ var About_text =
         '</td>' +
         '</tr>' +
         '<tr align="center">' +
-        '<td>GitHub</td>' +
-        '<td>Qiita</td>' +
+        '<td style="font-size:16px;">GitHub</td>' +
+        '<td style="font-size:16px;">Qiita</td>' +
         '</tr>' +
     '</table>'
 ;
 
 var Certification_text =
-    '<table border="0" height="100%">' +
-        '<tr>' +
-            '<td>2016/05&nbsp;&nbsp;</td><td>ITパスポート</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2016/09</td><td>c言語プログラミング能力認定試験 3級</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2016/11</td><td>基本情報技術者試験</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2017/06</td><td>LPIC 101 (740/800)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2017/07</td><td>LPIC 102 (640/800)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2018/02</td><td>MOS Excel 2013 Expert Part1 (1000/1000)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2018/03</td><td>MOS Word 2013 (1000/1000)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2018/07</td><td>ICND1 with English (919/1000)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2018/09</td><td>ICND2 with English (850/1000)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2018/11</td><td>応用情報技術者試験</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2019/06</td><td>LPIC 201 (720/800)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2019/07</td><td>LPIC 202 (640/800)</td>' +
-        '</tr>' +
-        '<tr>' +
-            '<td>2019/07</td><td>LPIC 304 (720/800)</td>' +
-        '</tr>' +
-    '</table>'
+    '2016/05&nbsp;&nbsp;ITパスポート<br />' +
+    '2016/09&nbsp;&nbsp;c言語プログラミング能力認定試験 3級<br />' +
+    '2016/11&nbsp;&nbsp;基本情報技術者試験<br />' +
+    '2017/06&nbsp;&nbsp;LPIC 101 (740/800)<br />' +
+    '2017/07&nbsp;&nbsp;LPIC 102 (640/800)<br />' +
+    '2018/02&nbsp;&nbsp;MOS Excel 2013 Expert Part1 (1000/1000)<br />' +
+    '2018/03&nbsp;&nbsp;MOS Word 2013 (1000/1000)<br />' +
+    '2018/07&nbsp;&nbsp;ICND1 with English (919/1000)<br />' +
+    '2018/09&nbsp;&nbsp;ICND2 with English (850/1000)<br />' +
+    '2018/11&nbsp;&nbsp;応用情報技術者試験<br />' +
+    '2019/06&nbsp;&nbsp;LPIC 201 (720/800)<br />' +
+    '2019/07&nbsp;&nbsp;LPIC 202 (640/800)<br />' +
+    '2019/07&nbsp;&nbsp;LPIC 304 (720/800)<br />'
 ;
