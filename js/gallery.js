389081e2e993780a2894213e5972d1e845f8560a
kumavale <kumavale.hyperion@gmail.com>
2022-08-17 11:07:47 +0900

    Change Cloudinary from GoogleDrive

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
-const content_url = "https://gist.githubusercontent.com/kumavale/a409860fc3e0b4fe828b31ecaf3f6b43/raw/CatGallery.json";
+const content_url = "https://gist.githubusercontent.com/kumavale/13100cdc5d451df2b27472aeedd61537/raw/CatGallery.json";
 
 document.addEventListener("DOMContentLoaded", async function() {
-    let lists = [];  // ファイルID一覧
+    let lists = [];  // ファイル名一覧
 
     await fetch(content_url, {
         mode: "cors",
@@ -14,11 +14,11 @@ document.addEventListener("DOMContentLoaded", async function() {
     let gallery = document.getElementById("gallery");
 
     for (let i = 0; i < lists.length; ++i) {
-        let fileid = lists[i];
-        let image  = document.createElement("img");
+        let filename = lists[i];
+        let image    = document.createElement("img");
 
         image.classList.add("image");
-        image.src = 'https://drive.google.com/uc?export=view&id=' + fileid + '&usp=sharing';
+        image.src = 'https://res.cloudinary.com/kumavale/image/upload/w_512/v1660699986/CatGallery/' + filename;
 
         gallery.appendChild(image);
     }
