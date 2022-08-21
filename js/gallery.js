2fde86ef0a2810feaca270e616336470a474441a
kumavale <kumavale.hyperion@gmail.com>
2022-08-21 18:24:16 +0900

    Fix gallery bugs and improve it

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,5 @@
 const content_url = "https://gist.githubusercontent.com/kumavale/13100cdc5d451df2b27472aeedd61537/raw/CatGallery.json";
+const image_size = "w_1024";
 
 document.addEventListener("DOMContentLoaded", async function() {
     let lists = [];  // ファイル名一覧
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", async function() {
         let image    = document.createElement("img");
 
         image.classList.add("image");
-        image.src = 'https://res.cloudinary.com/kumavale/image/upload/w_512/v1660699986/CatGallery/' + filename;
+        image.src = 'https://res.cloudinary.com/kumavale/image/upload/' + image_size + '/v1660699986/CatGallery/' + filename;
 
         gallery.appendChild(image);
     }
